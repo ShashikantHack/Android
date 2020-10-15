aa513298cd475886e445dff381cc768c0640a4f4
ShashikantHack <72934430+ShashikantHack@users.noreply.github.com>
2020-10-15 21:24:34 +0530

    Update MusicListAdapter.java

--- Turbo Share ( Admob )/app/src/main/java/com/techdudes/easysharing/adapter/MusicListAdapter.java
+++ Turbo Share ( Admob )/app/src/main/java/com/techdudes/easysharing/adapter/MusicListAdapter.java
@@ -114,7 +114,7 @@ public class MusicListAdapter
         return new GroupViewHolder(getInflater().inflate(R.layout.row_music, parent, false));
     }
 
-    @OverRide
+    @Override
     public void onBindViewHolder(@NonNull final GroupViewHolder holder, int position)
     {
         try {
@@ -137,7 +137,8 @@ public class MusicListAdapter
                             : object.albumHolder.title);
 
                     text3.setVisibility(View.GONE);
-                    textSeparator1.setVisibility(View.GONE); else {
+                    textSeparator1.setVisibility(View.GONE);
+                } else {
                     text2.setText(object.artist);
                     text3.setText(object.albumHolder.title);
                     text3.setVisibility(View.VISIBLE);
@@ -202,7 +203,7 @@ public class MusicListAdapter
                 return object.folder;
             else if (getGroupBy() == MODE_GROUP_BY_ALBUM)
                 return object.albumHolder.title;
-        
+        }
 
         return super.getSectionName(position, object);
     }
@@ -280,4 +281,5 @@ public class MusicListAdapter
         {
             return obj instanceof AlbumHolder ? ((AlbumHolder) obj).id == id : super.equals(obj);
         }
-
+    }
+}
